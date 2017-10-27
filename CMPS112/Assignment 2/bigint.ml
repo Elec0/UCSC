7441341397a30d55415cdeb7b822e506f2bd3130
Elec0 <elec08215@gmail.com>
2017-10-26 22:18:05 -0700

    Misc comments

--- CMPS112/Assignment 2/bigint.ml
+++ CMPS112/Assignment 2/bigint.ml
@@ -5,10 +5,11 @@ open Printf
 module Bigint = struct
 
     type sign     = Pos | Neg
-    type bigint   = Bigint of sign * int list
+    type bigint   = Bigint of sign * int list (* Is this just english? *)
     let  radix    = 10
     let  radixlen =  1
 
+	(* The random define stuff *)
     let car       = List.hd
     let cdr       = List.tl
     let map       = List.map
@@ -18,6 +19,7 @@ module Bigint = struct
     let strsub    = String.sub
     let zero      = Bigint (Pos, [])
 
+	(* String.toCharArray *)
     let charlist_of_string str = 
         let last = strlen str - 1
         in  let rec charlist pos result =
@@ -26,6 +28,7 @@ module Bigint = struct
             else charlist (pos - 1) (str.[pos] :: result)
         in  charlist last []
 
+	(* Bigint.parse(String) *)
     let bigint_of_string str =
         let len = strlen str
         in  let to_intlist first =
@@ -38,6 +41,7 @@ module Bigint = struct
                      then Bigint (Neg, to_intlist 1)
                      else Bigint (Pos, to_intlist 0)
 
+	(* Bigint.toString *)
     let string_of_bigint (Bigint (sign, value)) =
         match value with
         | []    -> "0"
@@ -46,6 +50,7 @@ module Bigint = struct
                        ((if sign = Pos then "" else "-") ::
                         (map string_of_int reversed))
 
+	(* Do the actual addition *)
     let rec add' list1 list2 carry = match (list1, list2, carry) with
         | list1, [], 0       -> list1
         | [], list2, 0       -> list2
@@ -55,19 +60,25 @@ module Bigint = struct
           let sum = car1 + car2 + carry
           in  sum mod radix :: add' cdr1 cdr2 (sum / radix)
 
+	(* Determine if adding or subtracting should be done *)
     let add (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
         if neg1 = neg2
         then Bigint (neg1, add' value1 value2 0)
         else zero
-
+	
+	(* Determine if subtracting or adding should be done *)
     let sub = add
 
+	(* Multiply numbers together *)
     let mul = add
 
+	(* Divide numbers *)
     let div = add
 
+	(* Get the remainder (modulus?) *)
     let rem = add
 
+	(* Raise number to a power *)
     let pow = add
 
 end
