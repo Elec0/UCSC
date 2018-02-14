6e63af50186a2fc58b393168bbc790e60a78e94f
Elec0 <elec08215@gmail.com>
2018-02-13 23:13:05 -0800

    Passed priority-condvar

--- CMPS111/Lab2/pintos/src/threads/condvar.c
+++ CMPS111/Lab2/pintos/src/threads/condvar.c
@@ -22,7 +22,7 @@
  * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+ * PURPOSE.    THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
  * BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO
  * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
  * MODIFICATIONS.
@@ -38,36 +38,36 @@
 #include "threads/thread.h"
 
 /* 
- * Initializes condition variable COND.  A condition variable
+ * Initializes condition variable COND.    A condition variable
  * allows one piece of code to signal a condition and cooperating
  * code to receive the signal and act upon it. 
  */
 void
 condvar_init(struct condvar *cond)
 {
-    ASSERT(cond != NULL);
-    list_init(&cond->waiters);
+        ASSERT(cond != NULL);
+        list_init(&cond->waiters);
 }
 
 /* 
  * Atomically releases LOCK and waits for COND to be signaled by
- * some other piece of code.  After COND is signaled, LOCK is
- * reacquired before returning.  LOCK must be held before calling
+ * some other piece of code.    After COND is signaled, LOCK is
+ * reacquired before returning.    LOCK must be held before calling
  * this function.
  *
  * The monitor implemented by this function is "Mesa" style, not
  * "Hoare" style, that is, sending and receiving a signal are not
- * an atomic operation.  Thus, typically the caller must recheck
+ * an atomic operation.    Thus, typically the caller must recheck
  * the condition after the wait completes and, if necessary, wait
  * again.
  *
  * A given condition variable is associated with only a single
  * lock, but one lock may be associated with any number of
- * condition variables.  That is, there is a one-to-many mapping
+ * condition variables.    That is, there is a one-to-many mapping
  * from locks to condition variables.
  *
  * This function may sleep, so it must not be called within an
- * interrupt handler.  This function may be called with
+ * interrupt handler.    This function may be called with
  * interrupts disabled, but interrupts will be turned back on if
  * we need to sleep. 
  */
@@ -87,6 +87,22 @@ condvar_wait(struct condvar *cond, struct lock *lock)
     lock_acquire(lock);
 }
 
+/*
+     The comparison function for list_insert_ordered, based on priority
+*/
+static bool condvar_less_sort (const struct list_elem *a_, const struct list_elem *b_,
+    void *aux UNUSED)
+{
+    struct semaphore *a = list_entry (a_, struct semaphore, elem);
+    struct semaphore *b = list_entry (b_, struct semaphore, elem);
+
+    // Compare the top priority values of the two semaphore lists: a->waiters[0], b->waiters[0]
+    struct thread *a_thread = list_entry(list_front(&a->waiters), struct thread, elem);
+    struct thread *b_thread = list_entry(list_front(&b->waiters), struct thread, elem);
+        
+    return a_thread->priority > b_thread->priority;
+}
+
 /* 
  * If any threads are waiting on COND(protected by LOCK), then
  * this function signals one of them to wake up from its wait.
@@ -104,14 +120,17 @@ condvar_signal(struct condvar *cond, struct lock *lock UNUSED)
     ASSERT(!intr_context());
     ASSERT(lock_held_by_current_thread(lock));
 
+
     if (!list_empty(&cond->waiters)) {
+        list_sort(&cond->waiters, condvar_less_sort, NULL);
         semaphore_up(list_entry(list_pop_front(&cond->waiters), struct semaphore, elem));
+        thread_yield();
     }
 }
 
 /* 
  * Wakes up all threads, if any, waiting on COND(protected by
- * LOCK).  LOCK must be held before calling this function.
+ * LOCK).    LOCK must be held before calling this function.
  *
  * An interrupt handler cannot acquire a lock, so it does not
  * make sense to try to signal a condition variable within an
