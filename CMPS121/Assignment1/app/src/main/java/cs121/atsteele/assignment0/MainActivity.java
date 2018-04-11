193dc53b96c26b6db143fc51c98b16b8faa4fea3
Elec0 <elec08215@gmail.com>
2018-04-10 17:08:17 -0700

    (Hopefully) finished assignment 1

--- CMPS121/Assignment1/app/src/main/java/cs121/atsteele/assignment0/MainActivity.java
+++ CMPS121/Assignment1/app/src/main/java/cs121/atsteele/assignment0/MainActivity.java
@@ -16,9 +16,9 @@ import java.text.DecimalFormat;
 public class MainActivity extends AppCompatActivity
 {
 
-    TextView lblPow2Output;
-    EditText txtPow2;
-    Button btnPow2;
+    TextView lblPow2Output, lblPow3Output, lblPow4Output, lblPow5Output;
+    EditText txtPow2, txtPow3, txtPow4, txtPow5;
+    Button btnPow2, btnPow3, btnPow4, btnPow5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,13 +30,42 @@ public class MainActivity extends AppCompatActivity
         txtPow2 = (EditText) findViewById(R.id.txtPow2);
         btnPow2 = (Button) findViewById(R.id.btnPow2);
 
+        lblPow3Output = (TextView) findViewById(R.id.lblPow3Output);
+        txtPow3 = (EditText) findViewById(R.id.txtPow3);
+        btnPow3 = (Button) findViewById(R.id.btnPow3);
+
+        lblPow4Output = (TextView) findViewById(R.id.lblPow4Output);
+        txtPow4 = (EditText) findViewById(R.id.txtPow4);
+        btnPow4 = (Button) findViewById(R.id.btnPow4);
+
+        lblPow5Output = (TextView) findViewById(R.id.lblPow5Output);
+        txtPow5 = (EditText) findViewById(R.id.txtPow5);
+        btnPow5 = (Button) findViewById(R.id.btnPow5);
 
 
         // All button listeners here
         btnPow2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setOutput(txtPow2, lblPow2Output);
+                setOutput(txtPow2, lblPow2Output, 2);
+            }
+        });
+        btnPow3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOutput(txtPow3, lblPow3Output, 3);
+            }
+        });
+        btnPow4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOutput(txtPow4, lblPow4Output, 4);
+            }
+        });
+        btnPow5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOutput(txtPow5, lblPow5Output, 5);
             }
         });
     }
@@ -47,13 +76,13 @@ public class MainActivity extends AppCompatActivity
      * @param text
      * @param view
      */
-    private void setOutput(EditText text, TextView view)
+    private void setOutput(EditText text, TextView view, int pow)
     {
         try
         {
             double v = Double.parseDouble(text.getText().toString());
-            double res = Math.pow(2, v);
-            view.setText(new DecimalFormat("#.##").format(res));
+            double res = Math.pow(pow, v);
+            view.setText(String.format("%.2f", res));
         }
         catch(Exception e)
         {}
