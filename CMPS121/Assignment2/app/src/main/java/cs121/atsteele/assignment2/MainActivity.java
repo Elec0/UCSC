46f7e43aefb12b73d50a3a8cf4bf6032620aab49
Elec0 <elec08215@gmail.com>
2018-05-07 15:25:45 -0700

    Add action menu button

--- CMPS121/Assignment2/app/src/main/java/cs121/atsteele/assignment2/MainActivity.java
+++ CMPS121/Assignment2/app/src/main/java/cs121/atsteele/assignment2/MainActivity.java
@@ -1,7 +1,11 @@
 package cs121.atsteele.assignment2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    // handle button activities
+    // Same as onCreateOptionsMenu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_addevent) {
+            // Start the add intent
+            Intent intent = new Intent(this, EventAddActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // create an action bar button
+    // https://stackoverflow.com/questions/38158953/how-to-create-button-in-action-bar-in-android
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
