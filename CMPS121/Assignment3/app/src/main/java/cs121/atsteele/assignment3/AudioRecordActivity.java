eb2b28b3e67af6b4bf522925e4b6086ab78c9536
Elec0 <elec08215@gmail.com>
2018-05-30 20:15:10 -0700

    121 Assignment 3 finished

--- CMPS121/Assignment3/app/src/main/java/cs121/atsteele/assignment3/AudioRecordActivity.java
+++ CMPS121/Assignment3/app/src/main/java/cs121/atsteele/assignment3/AudioRecordActivity.java
@@ -1,15 +1,126 @@
 package cs121.atsteele.assignment3;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+import java.io.IOException;
 
 public class AudioRecordActivity extends AppCompatActivity
 {
+    public static final int REQUEST_RECORD_AUDIO_PERMISSION = 113;
+    private String FILE_LOC = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_record);
+
+        // From https://stackoverflow.com/questions/10472839/using-javascript-in-android-webview
+        WebView wv = findViewById(R.id.webAudio);
+        wv.getSettings().setJavaScriptEnabled(true);
+        // register class containing methods to be exposed to JavaScript
+        AudioJavascriptInterface Android = new AudioJavascriptInterface(this);
+        wv.addJavascriptInterface(Android, "Android");
+
+        wv.loadUrl("https://users.soe.ucsc.edu/~dustinadams/CMPS121/assignment3/www/index.html");
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        FILE_LOC = getExternalCacheDir().getAbsolutePath() + "/";
+    }
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted )
+            finish();
+
+    }
+
+    public class AudioJavascriptInterface {
+        Context mContext;
+        MediaRecorder mRecorder;
+        MediaPlayer mPlayer;
+
+        public AudioJavascriptInterface(Context c) {
+            mContext = c;
+        }
+
+        @android.webkit.JavascriptInterface
+        public void record() {
+            SharedPreferences settings = getSharedPreferences(MainActivity.PREFS_NAME, 0);
+            int numRecordings  = settings.getInt(MainActivity.PREFS_NUM_RECS, 0) + 1;
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt(MainActivity.PREFS_NUM_RECS, numRecordings);
+            editor.apply();
+            String mFileName = FILE_LOC + "Audio" + numRecordings + ".3gp";
+
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setOutputFile(mFileName);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {  }
+
+            mRecorder.start();
+        }
+
+        @android.webkit.JavascriptInterface
+        public void stop() {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        @android.webkit.JavascriptInterface
+        public void play() {
+            mPlayer = new MediaPlayer();
+            try {
+                SharedPreferences settings = getSharedPreferences(MainActivity.PREFS_NAME, 0);
+                int numRecordings  = settings.getInt(MainActivity.PREFS_NUM_RECS, 0);
+                String mFileName = FILE_LOC + "Audio" + numRecordings + ".3gp";
+
+                mPlayer.setDataSource(mFileName);
+                mPlayer.prepare();
+                mPlayer.start();
+
+            } catch (IOException e) {  }
+        }
+
+        @android.webkit.JavascriptInterface
+        public void stoprec() {
+            mPlayer.release();
+            mPlayer = null;
+        }
+
+        @android.webkit.JavascriptInterface
+        public void exit() {
+            finish();
+        }
     }
 }
--- CMPS121/Assignment3/app/src/main/java/cs121/atsteele/assignment3/MainActivity.java
+++ CMPS121/Assignment3/app/src/main/java/cs121/atsteele/assignment3/MainActivity.java
@@ -1,15 +1,106 @@
 package cs121.atsteele.assignment3;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity
 {
+    public static final String PREFS_NAME = "Asn3Prefs";
+    public static final String PREFS_NUM_RECS = "numRecordings";
+
+    private ListView audioList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateList();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateList();
+    }
+
+    // handle button activities
+    // Same as onCreateOptionsMenu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_addaudio) {
+            // Start the add intent
+            Intent intent = new Intent(this, AudioRecordActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // create an action bar button
+    // https://stackoverflow.com/questions/38158953/how-to-create-button-in-action-bar-in-android
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * We're reading the int from SharedPreferences and putting that many elements into the
+     * list, then we can handle actually playing them later.
+     */
+    private void populateList() {
+        audioList = findViewById(R.id.lstMain);
+        // Clear the list
+        audioList.setAdapter(null);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        int numRecordings  = settings.getInt(PREFS_NUM_RECS, 0);
+
+        String[] labels;
+
+        if(numRecordings <= 0)
+            labels = new String[] {"No Voice Memos"};
+        else
+           labels = new String[numRecordings];
+
+        for(int i = 0; i < numRecordings; ++i)
+        {
+            labels[i] = "Audio Recording " + (i+1);
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, labels);
+        //mListView is the name of our ListView object
+
+        audioList.setAdapter(adapter);
+
+        audioList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                MediaPlayer mPlayer = new MediaPlayer();
+                try {
+                    String mFileName = getExternalCacheDir().getAbsolutePath() + "/" + "Audio" + (i+1) + ".3gp";
+
+                    mPlayer.setDataSource(mFileName);
+                    mPlayer.prepare();
+                    mPlayer.start();
+
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, "Audio" + (i+1) + ".3gp not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
